028fc38e57c9d0e409173b6e5f6763071a31cf84
Julian <cusco2891@gmail.com>
2019-10-31 03:02:42 -0400

    api not working

--- server.js
+++ server.js
@@ -5,20 +5,27 @@ const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const axios = require('axios');
 
+// var logger = require("morgan");
 // const request = require("request");
 
 const app = express();
 
 
+// Use morgan logger for logging requests
+// app.use(logger("dev"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
 
 var PORT = process.env.PORT || 8080;
 
-
-app.use(express.static("public"));
-
 require("./routes/htmlRoutes")(app);
-// require("./routes/apiRoutes")(app);
-
+require("./routes/apiRoutes")(app);
 
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
